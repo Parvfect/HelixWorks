42336847a67c6e12215ad28b917880c84550b38a
Parvfect <parvagrw02@gmail.com>
2023-11-13 12:46:08 +0000

    Added code saving functions

---
+++ simulate_save_G.py
@@ -0,0 +1,31 @@
+
+from Hmatrixbaby import ParityCheckMatrix
+import row_echleon as r
+import uuid
+import os
+import numpy as np
+
+dv, dc, k, n, ffdim = 3, 6, 500, 1000, 67
+PM = ParityCheckMatrix(dv, dc, k, n, ffdim=ffdim)
+Harr = PM.get_H_arr()
+H = PM.createHMatrix(Harr=Harr)
+G = r.parity_to_generator(H, ffdim=ffdim)
+
+if np.any(np.dot(G, H.T) % ffdim != 0):
+    print("Matrices are not valid, aborting simulation")
+    exit()
+
+unique_filename = str(uuid.uuid4())
+
+filename = "codes/dv_dc_k_n_ffdim={}_{}_{}_{}_{}/{}".format(dv, dc, k, n, ffdim, unique_filename)
+
+# Create the directory if it does not exist
+if not os.path.exists(filename):
+    os.makedirs(filename)
+
+# Save the Harr, H and G matrices
+np.save(filename + "/Harr", Harr)
+np.save(filename + "/H", H)
+np.save(filename + "/G", G)
+
+print("Saved the matrices to {}".format(filename))
---
+++ validate_saved_codes.py
@@ -0,0 +1,14 @@
+
+
+import numpy as np
+
+
+# Load a saved code
+
+
+
+H = np.load(f"C:\Users\Parv\Doc\HelixWorks\code\codes\dvdckn36100200\2a0c2c86-d507-43fb-bcd4-43ad715a071a\H.npy")
+G = np.load(f"C:\Users\Parv\Doc\HelixWorks\code\codes\dvdckn36100200\2a0c2c86-d507-43fb-bcd4-43ad715a071a\G.npy")
+
+if np.any(np.matmul(H, G.T) != 0):
+    print("H and G are not orthogonal")
